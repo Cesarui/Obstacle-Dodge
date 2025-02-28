c52f2856bda079e93cbd5bd52739ead88dbd4db0
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-02-28 17:10:20 -0500

    ObjectHitCollider, Scorer & Time added

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Prints out time on attached object
+        Debug.Log(Time.time);
+    }
+
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,24 +7,36 @@ public class Movement : MonoBehaviour
     // Serializing allows you to see the variable in the view port
     // So no need to have public like I thought
 
-    
+
 
     void Start()
     {
-        
+        PrintInstruction();
     }
 
 
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Press WASD or Arrow keys to move around");
+        Debug.Log("Your job is to dodge as many obstacles as you can:)");
+    }
+    void MovePlayer()
+    {
         /* Multiply your x input which is 1 or -1, then times it by deltaTime
-           That makes sure fps is independent, which allows the moveSpeed to be consistent across
-           All devices! 
-        */
+          That makes sure fps is independent, which allows the moveSpeed to be consistent across
+          All devices! 
+       */
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue , yValue, zValue);
+
+        transform.Translate(xValue, yValue, zValue);
     }
 }
