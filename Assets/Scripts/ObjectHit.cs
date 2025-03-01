4bd2e21b3512085efb6a357af5202f036b2cc567
Cesar Pimentel <cpimentelortiz23@gmail.com>
2025-03-01 15:45:10 -0500

    Comments added for new scripts

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class ObjectHit : MonoBehaviour
 {
-
+    // When the objects collider senses another object going in it, it'll change its color
     void OnCollisionEnter(Collision collision)
     {
         GetComponent<MeshRenderer>().material.color = Color.black; 
--- Assets/Scripts/Scorer.cs
+++ Assets/Scripts/Scorer.cs
@@ -4,6 +4,7 @@ public class Scorer : MonoBehaviour
 {
     int hits = 0;
 
+    // Logic to track when player hits an object and how many times
     private void OnCollisionEnter(Collision other)
     {
         hits++;
